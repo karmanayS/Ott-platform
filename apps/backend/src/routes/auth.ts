89085c53d5ce39e5575d83d6167139fbaaa23d42
Karmanay Sharma <karmanaysharmaog@gmail.com>
2025-11-25 22:02:49 +0530

    userauthv0

---
+++ apps/backend/src/routes/auth.ts
@@ -0,0 +1,111 @@
+import express, { Router } from "express"
+import { JWT_SECRET, signinSchema, signupSchema } from "@repo/common/schemas"
+import prisma from "@repo/db/client";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export const authRouter:Router = express.Router()
+
+authRouter.post("/signup",async(req,res) => {
+    const {username , email ,password} = req.body
+    const result = signupSchema.safeParse({
+        username,email,password
+    })
+    if (!result.success) return res.status(403).json({
+        success: false,
+        message: JSON.parse(result.error.message)[0].message
+    })
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        const hashedPassword = await bcrypt.hash(password,10)
+        const otp = Math.floor(100000 + Math.random() * 900000).toString()
+        const otpExpiryDate = new Date()
+        otpExpiryDate.setHours(otpExpiryDate.getHours() + 1)
+
+        if (user) {
+            if (user.isVerified) return res.status(409).json({
+                success: false,
+                message: "verified user already exists"
+            })
+            await prisma.user.update({
+                where: {
+                    email
+                }, data: {
+                    username,
+                    password: hashedPassword,
+                    otp,
+                    otpExpiry: otpExpiryDate
+                }
+            })
+            
+            //send email with otp
+            return res.json({
+                success:true,
+                message: "Sent verification email"
+            })
+        }
+        await prisma.user.create({
+            data: {
+                username,email,
+                password:hashedPassword,
+                otp,
+                otpExpiry: otpExpiryDate
+            }
+        })
+        //send email with otp
+        return res.json({
+            success:true,
+            message: "Sent verification email"
+        })
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json({
+            success: false,
+            message: "Error while signing up"
+        })
+    }    
+
+})
+
+authRouter.post("/signin",async(req,res) => {
+    const { email,password } = req.body
+    const result = signinSchema.safeParse({
+        email,password
+    })
+    if (!result.success) return res.status(403).json({
+        success: false,
+        message: JSON.parse(result.error.message)[0].message
+    })
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email,password
+            }
+        })
+        if (!user) return res.json({
+            success:false,
+            message: "User not found"
+        })
+        if (!user.isVerified) return res.json({
+            success: false,
+            message: "User is not verified, please signup"
+        })
+        const token = jwt.sign({userId:user.id}, JWT_SECRET)
+        res.json({
+            success: true,
+            token,
+            message: "Signed in successfully"
+        })
+    } catch (err) {
+        console.log(err)
+        return res.json({
+            success: false,
+            message: "Error while signing in"
+        })
+    }    
+})
